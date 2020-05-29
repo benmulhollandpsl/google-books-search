c5eadfb69f35688f4deea2b24c18101000d36f67
Ben Mulholland <ben.mulholland@gmail.com>
2020-05-29 08:57:39 -0400

    tried making search component, need to define components and states

---
+++ src/Search.js
@@ -0,0 +1,65 @@
+import React from "react";
+import "./App.css";
+import axios from'axios';
+
+
+//where Search field will appear
+class Search extends Component {
+    state = {
+        books:[]
+    };
+  
+      componentDidMount() {
+          this.setBook();
+      }
+      
+      setBook = () => {
+      const [books, setBook] = useState("");
+      const [result, setResult] = useState([]);
+      const [apiKey, setApiKey] = useState("AIzaSyDacCrRbdlrbik-iyxN9QAdMjH7-wALY08");
+      }
+      handleChange = (event) => { 
+        
+        const book = event.target.value;
+  
+        setBook(book);
+      }
+      
+      handleSubmit = (event) => {
+          event.preventDefault();
+          console.log(book)
+  
+      }
+    
+      
+    render() {
+      return (
+       <div className="App">
+           <div className="App-header">
+            
+            <h2>GoogleBooks React</h2>
+          </div>
+          <p className="App-intro">
+            To get started, edit <code>src/App.js</code> and save to reload.
+          </p>
+        
+  
+  
+      <div class="container">
+        <form>
+        <div class="form-group">
+        <input type="text" 
+          className="input-control mt-10" 
+          placeholder="book search" 
+          autoComplete="off"/>
+        </div>
+        <button type="submit" className="btn btn-danger">Search</button>
+        </form>
+        </div>
+        </div>
+      );
+    }
+  }
+  
+  export default Search;
+  
