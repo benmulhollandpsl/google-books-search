ea54b669b011d0cc7c3c237ab5490301b4c9d668
Ben Mulholland <ben.mulholland@gmail.com>
2020-05-29 08:28:26 -0400

    app.js, installed axios, added api key, added booksearch functions and building change and consolelogs next

--- google-books-search/src/App.js
+++ google-books-search/src/App.js
@@ -1,18 +1,54 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import axios from'axios';
 
 class App extends Component {
+  
+  
   render() {
+    
+    const [books, setBook] = useState("");
+    const[result, setResult] = useState([]);
+    const [apiKey, setApiKey] = useState("AIzaSyDacCrRbdlrbik-iyxN9QAdMjH7-wALY08");
+    
+    handleChange = (event) => { 
+      
+      const book = event.target.value;
+
+      setBook(book);
+    }
+    
+    handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(book)
+
+    }
+    
+    
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+     <div className="App">
+         <div className="App-header">
+          
+          <h2>GoogleBooks React</h2>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+      
+
+
+    <div class="container">
+      <form>
+      <div class="form-group">
+      <input type="text" 
+        className="input-control mt-10" 
+        placeholder="book search" 
+        autoComplete="off"/>
+      </div>
+      <button type="submit" className="btn btn-danger">Search</button>
+      </form>
+      </div>
       </div>
     );
   }
--- google-books-search/src/index.js
+++ google-books-search/src/index.js
@@ -2,7 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import registerServiceWorker from "./registerServiceWorker";
+// import registerServiceWorker from "./registerServiceWorker";
+import axios from 'axios';
 
 ReactDOM.render(<App />, document.getElementById("root"));
-registerServiceWorker();
+// registerServiceWorker();
+
